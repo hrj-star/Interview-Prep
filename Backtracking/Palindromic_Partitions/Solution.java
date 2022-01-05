90800e0df8b6088a48f7eb468a77a0219e1d139a
hrj-star <hetjoshi022@gmail.com>
2022-01-05 19:01:00 +0530

    Some changes

--- Backtracking/Palindromic_Partitions/Solution.java
+++ Backtracking/Palindromic_Partitions/Solution.java
@@ -15,46 +15,47 @@ class Solution {
   public static boolean isPalindrome(String str){
     int left = 0 , right = str.length() - 1;
     while(left < right){
-      if(str.charAt(left++)  == str.charAt(right--)){
-        return true;
+      char leftchar = str.charAt(left);
+      char rightchar = str.charAt(right);
+
+      if(leftchar  != rightchar){
+        return false;
       }
+
+      left++;
+      right--;
     } // while end 
-      return false;
+      return true;
   }
 
 
 
-  public static void palindromic_partition(String str , String so_far  ){
+  public static void palindromic_partition(String str , String so_far){
     
-    // base check - check for empty string ,return empty
+    // base check - check for empty string / length reached till end and string exhausted  ,return so_far
 
-    int start = 0 , end = str.length()-1;
-
-    if(start == end){
+    if(str.length()== 0){
       System.out.println(so_far);
-      return ;
+      return;
     }
 
     for(int i=0 ; i < str.length() ; i++){
       String prefix = str.substring(0,i+1);
-      String remaining = str.substring(i);
+      String remaining = str.substring(i+1);
 
       if(isPalindrome(prefix)){
-       
         palindromic_partition(remaining,so_far+ "(" +prefix + ")");
-
       }
     }
-   
   }
   
   public static void main(String[] args) {
 
     String str1 = "nitin";
-    String str2 = "abaaba"; 
+    String str2 = "aab"; 
 
-    palindromic_partition(str1," ");
-    palindromic_partition(str2," ");
+    palindromic_partition(str1,"");
+    palindromic_partition(str2,"");
    
   }
 }
